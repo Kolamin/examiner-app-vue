252562583dfec41b05904ec3d4a780820d3fe90c
Anton Zhiliakov <gofranich@gmail.com>
2022-10-20 12:48:20 +0600

    add select count

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,25 @@
 <template>
   <div v-if="user">
-    <div>
+    <div class="select">
       <label for="sect">Выберите раздел:</label>
       <select id="sect" v-model="section">
         <option value="questions">А.1. Основы промышленной безопасности</option>
         <option value="hquestions">
           ЭБ 172.15. Эксплуатация тепловых энергоустановок и тепловых сетей
         </option>
+      </select>
+      <label for="count_page">Показывать вопросов:</label>
+      <select id="count_page" v-model="perPage">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
       </select>
     </div>
     <div v-if="section !== undefined">
@@ -87,7 +100,7 @@ export default {
       page.value = pageNumber;
     }
 
-    watch([section, page], (newValue, prevValue) => {
+    watch([section, page, perPage], (newValue, prevValue) => {
       console.log(newValue, prevValue);
       fetchQuestions();
     });
@@ -105,3 +118,12 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.select {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-basis: auto;
+}
+</style>
